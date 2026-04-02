03c41354e7fc55f9633335cbb4ac70ece7e1323f
kufx <144138627+kufx@users.noreply.github.com>
2026-04-02 12:57:36 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '336a7a4ad19180be9010f525c9a6d740',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: '酷小呵笔记',
+  domain: 'notes.kuhehe.top',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '酷小呵的资源笔记',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
