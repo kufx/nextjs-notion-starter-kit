07b591a94cb49bd9321a66ebb4c9a8a409a39d4b
kufx <144138627+kufx@users.noreply.github.com>
2026-04-02 13:35:07 +0800

    Update get-canonical-page-id.ts

--- lib/get-canonical-page-id.ts
+++ lib/get-canonical-page-id.ts
@@ -18,6 +18,6 @@ export function getCanonicalPageId(
     return override
   } else {
     // 只改了这里！直接返回纯ID，不拼标题
-    return parsePageId(pageId) ?? undefined
+    return parsePageId(pageId, { uuid: false }) ?? undefined
   }
 }
